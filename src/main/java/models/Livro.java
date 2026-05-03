daa523bc568ddcf35e69599225c2b471265d4366
eduardasxntos <oliveiramariaeduarda065@gmail.com>
2026-05-03 11:26:30 -0300

    Update Livro.java
    Atualização de models e Repository; adequação de services às novas orientações

--- src/main/java/models/Livro.java
+++ src/main/java/models/Livro.java
@@ -4,64 +4,102 @@ import java.time.LocalDate;
 
 public class Livro {
 
-    private boolean disponivel;
-
+    private Long id;
     private String nome;
-    private String isbn;
     private String autor;
+    private String isbn;
     private String genero;
     private String descricao;
     private LocalDate dataPublicacao;
-    private static long idCount=0;
-    private long id;
+    private boolean disponivel;
 
-    public Livro(String nome, String isbn, String genero, boolean disponivel, String descricao,
-                 LocalDate dataPublicacao) {
+    // Construtor
+    public Livro(Long id, String nome, String autor, String isbn, String genero,
+                 String descricao, LocalDate dataPublicacao) {
+
+        this.id = id;
         this.nome = nome;
+        this.autor = autor;
         this.isbn = isbn;
         this.genero = genero;
-        this.disponivel = disponivel;
         this.descricao = descricao;
         this.dataPublicacao = dataPublicacao;
-        this.id= ++idCount;
+        this.disponivel = true;
     }
 
-    // Getters
+    // --- Getters ---
     public Long getId() {
         return id;
-    }
-
-    public LocalDate getDataPublicacao() {
-        return dataPublicacao;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public String getIsbn() {
-        return isbn;
     }
 
     public String getNome() {
         return nome;
     }
 
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDate getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    // Retorna se o livro está disponível
     public boolean isDisponivel() {
         return disponivel;
     }
 
+    // Define se o livro está disponível
     public void setDisponivel(boolean disponivel) {
         this.disponivel = disponivel;
     }
 
+    // Compara dois livros pelo id
+    @Override
+    public boolean equals(Object obj) {
 
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Livro outro = (Livro) obj;
+
+        if (this.id == null) {
+            return false;
+        }
+
+        return this.id.equals(outro.id);
+    }
+
+    // Gera código hash baseado no id
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    // Retorna representação em texto
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", disponivel=" + disponivel +
+                '}';
+    }
 }
