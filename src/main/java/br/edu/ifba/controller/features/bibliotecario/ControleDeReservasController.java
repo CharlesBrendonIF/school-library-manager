6abb4e791bcae819090c9bd47f03946dbd08dc86
nicolas237 <nicolas@focomultimidia.com>
2026-05-06 01:28:49 -0300

    feat: se aplicado, renomeia controleDeReservasController e implementa método initialize

---
+++ src/main/java/br/edu/ifba/controller/features/bibliotecario/ControleDeReservasController.java
@@ -0,0 +1,13 @@
+package br.edu.ifba.controller.features.bibliotecario;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControleDeReservasController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+            System.out.println("Controle de Reservas inicializado");
+    }
+}
