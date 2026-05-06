6dd3ec3f23f38baf14e0b2568c879bb81ec5ad7f
nicolas237 <nicolas@focomultimidia.com>
2026-05-06 01:29:04 -0300

    feat: se aplicado, implementa a interface Initializable no InventarioController e adiciona método initialize

--- src/main/java/br/edu/ifba/controller/features/bibliotecario/InventarioController.java
+++ src/main/java/br/edu/ifba/controller/features/bibliotecario/InventarioController.java
@@ -1,4 +1,13 @@
 package br.edu.ifba.controller.features.bibliotecario;
 
-public class InventarioController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InventarioController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Inventário do Bibliotecário inicializado");
+    }
 }
